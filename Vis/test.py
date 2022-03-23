7e25462d519ccd1d75023e3c5e53d0be362c41b0
colinauyeung <daisuke.moto403@gmail.com>
2022-03-23 00:21:32 -0600

    deleted openssl

--- Vis/test.py
+++ Vis/test.py
@@ -1,4 +1,6 @@
 import json
+from time import sleep
+from progress.bar import ChargingBar
 
 with open('listedhandles.json', encoding="utf-8") as f:
     js = json.load(f)
@@ -10,3 +12,11 @@ for key in js:
     for riding in js[key]:
         total = total + len(js[key][riding])
 print(total)
+bar = ChargingBar("Processing", max =total)
+for key in js:
+    for riding in js[key]:
+        for i in js[key][riding]:
+            sleep(0.1)
+            bar.next()
+
+bar.finish()
