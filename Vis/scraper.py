4fbacedb6f53aaa685430cb21c3b51f5333675a7
Colin Au Yeung <daisuke.moto403@gmail.com>
2022-03-23 20:31:33 -0600

    Partial outputs

--- Vis/scraper.py
+++ Vis/scraper.py
@@ -1,7 +1,9 @@
 import snscrape.modules.twitter as sntwitter
 from progress.bar import ChargingBar
 import json
+import time
 import csv
+
 
 with open('listedhandles.json', encoding="utf-8") as f:
     js = json.load(f)
@@ -9,56 +11,65 @@ total = 0;
 
 
 
-with open('output.csv', mode="w", encoding="utf-8") as csv_file:
-    fieldnames = ['id', 'name', 'winner', 'Incumbent', 'party','District','username', 'date', 'messagetext', 
-                    'lengthofmessage',
-                    'retweets', 'replies', 'quotes',
-                    'likes', 'photo', 'numofphoto',
-                    'gif', 'video', 'ReplyingTo','ReplyToID',
-                    'mentionedusers', 'numofmentions',
-                    'hashtags',"numofhashtags"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writerow({'id':'id', 
-                    'name':'name', 
-                    'winner':'winner', 
-                    'Incumbent':'incumbent', 
-                    'party': 'party',
-                    'District':'district',
-                    'username': 'username', 
-                    'date': 'date', 
-                    'messagetext': 'message',
-                    'lengthofmessage':'lengthofmessage', 
-                    'retweets':'retweets', 
-                    'replies':'replies', 
-                    'quotes':'quotes',
-                    'likes':'likes', 
-                    'photo':'hasphoto', 
-                    'numofphoto':'numofphoto',
-                    'gif':'hasgif', 
-                    'video':'hasvideo', 
-                    'ReplyingTo':'ReplyingTo',
-                    'ReplyToID':'ReplyToID',
-                    'mentionedusers':'mentionedusers', 
-                    'numofmentions':'numofmentions',
-                    'hashtags':'hashtags', 
-                    "numofhashtags": "numofhashtags"})
+
     
-    count = 0
-    for key in js:
+    # count = 0
+    # for key in js:
+    #     for riding in js[key]:
+    #         count = count + len(js[key][riding])
+    # bar = ChargingBar("Processing", max =count)
+ignore =['Mississippi', 'Oklahoma', 'Minnesota', 'Alaska At-Large', 'Arkansas', 'New Mexico', 'Virgin Islands At-Large', 'Indiana', 'Maryland', 'Louisiana', 'Idaho', 'American Samoa At-Large', 'Arizona', 'Iowa', 'Montana At-Large', 'Michigan', 'Kansas', 'Utah', 'Virginia', 'Oregon', 'District of Columbia At-Large', 'Connecticut', 'Tennessee', 'California', 'Massachusetts', 'Vermont At-Large', 'West Virginia', 'South Carolina']
+for key in js:
+    if(key in ignore):
+        continue
+    with open('output'+key+'.csv', mode="w", encoding="utf-8") as csv_file:
+        fieldnames = ['id', 'name', 'winner', 'Incumbent', 'party','District','username', 'date', 'messagetext', 
+                        'lengthofmessage',
+                        'retweets', 'replies', 'quotes',
+                        'likes', 'photo', 'numofphoto',
+                        'gif', 'video', 'ReplyingTo','ReplyToID',
+                        'mentionedusers', 'numofmentions',
+                        'hashtags',"numofhashtags"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writerow({'id':'id', 
+                        'name':'name', 
+                        'winner':'winner', 
+                        'Incumbent':'incumbent', 
+                        'party': 'party',
+                        'District':'district',
+                        'username': 'username', 
+                        'date': 'date', 
+                        'messagetext': 'message',
+                        'lengthofmessage':'lengthofmessage', 
+                        'retweets':'retweets', 
+                        'replies':'replies', 
+                        'quotes':'quotes',
+                        'likes':'likes', 
+                        'photo':'hasphoto', 
+                        'numofphoto':'numofphoto',
+                        'gif':'hasgif', 
+                        'video':'hasvideo', 
+                        'ReplyingTo':'ReplyingTo',
+                        'ReplyToID':'ReplyToID',
+                        'mentionedusers':'mentionedusers', 
+                        'numofmentions':'numofmentions',
+                        'hashtags':'hashtags', 
+                        "numofhashtags": "numofhashtags"})
+        count = 0
         for riding in js[key]:
-            count = count + len(js[key][riding])
-    bar = ChargingBar("Processing", max =count)
-    
-    for key in js:
+            for i in range(0, len(js[key][riding])):
+                count = count + len(js[key][riding][i]["Twitter"])
+        bar = ChargingBar(key, max =count)
+
         for riding in js[key]:
             for i in range(0, len(js[key][riding])):
                 for handle in js[key][riding][i]["Twitter"]:
                     # print(js[key][riding][i])
                     # Creating list to append tweet data to
 
-
+                    # print(handle)
                     # Using TwitterSearchScraper to scrape data and append tweets to list
-                    for j,tweet in enumerate(sntwitter.TwitterSearchScraper('from:{0} since:2020-09-3 until:2020-11-3'.format(handle)).get_items()):
+                    for j,tweet in enumerate(sntwitter.TwitterSearchScraper('from:{0} since:2020-9-3 until:2020-11-3'.format(handle)).get_items()):
                         # if i>50:
                         #     break
                         video = False
@@ -105,7 +116,7 @@ with open('output.csv', mode="w", encoding="utf-8") as csv_file:
                         district = district.upper()
 
 
-               
+            
                         hash = ""
                         hashnum = 0
                         if(type(mytweet["hashtags"]) != type(None)):
@@ -158,10 +169,15 @@ with open('output.csv', mode="w", encoding="utf-8") as csv_file:
                             'hashtags': hash, 
                             "numofhashtags": hashnum}
                         )
+                    bar.next()
+                    time.sleep(5);
 
                     # "retweetedtweet":tweet.retweetedTweet, "quotedTweet":tweet.quotedTweet,
-                bar.next()
-                    
+                
+        bar.finish()
+        ignore.append(key)
+        print(ignore)
+                
 
 
 
@@ -175,7 +191,6 @@ with open('output.csv', mode="w", encoding="utf-8") as csv_file:
     #         break
     # if(total > 500):
     #     break
-    bar.finish()
 # print(total)
 
 # with open("outmini.json", "w") as outfile:
